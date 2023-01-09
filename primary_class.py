1029c05caa9861acbf9676e8f16153dfebbe1141
Dams4K <dams4k@users.noreply.github.com>
2023-01-09 20:06:32 +0100

    begging

--- primary_class.py
+++ primary_class.py
@@ -1,8 +1,20 @@
+import os
 import json
 
 class Saveable:
     def __init__(self, path):
         self.to_save = []
 
-if __name__ == "__main__":
-    pass
+    def retrieve_data(self):
+        return {attr_name: getattr(self, attr_name) for attr_name in self.to_save}
+
+    def save(self):
+        pass
+    def load(self):
+        pass
+
+    
+    def create_dirs(self):
+        dirname = os.path.dirname(self.file_path)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
